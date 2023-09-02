f139df85ab89435c1c24ef9dc782d9f282f07031
Jly4 <iilya.mironov@yandex.ru>
2023-09-02 19:51:37 +0700

    Configs

--- main.py
+++ main.py
@@ -1,27 +1,43 @@
 import re
+import sys
 import time
-import configs
-import calendar
-import pandas as pd
-from datetime import datetime
-from aiogram import Bot, Dispatcher, types
+import asyncio
+import logging
 #import configs
-import temp_configs as configs
+import temp.temp_configs as configs
+import pandas as pd
+from os import getenv
+from datetime import datetime
+from aiogram import Bot, Dispatcher, Router, types
+from aiogram.filters import Command
+from aiogram.types import Message
 
+# Создаем объекты бота и диспетчера
+bot: Bot = Bot(token=configs.token)
+dp: Dispatcher = Dispatcher()
+
+
+
+
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
+
+"""
 local_date = datetime(year=2023, month=9, day=1)
-bot = Bot(token=configs.token)
-dp = Dispatcher(bot)
 
+# schedule parsing
 while True:
     last_edit = ''
-    shedule = pd.read_html("https://web.archive.org/web/20221002202403/https://lyceum.tom.ru/raspsp/index.php", encoding="cp1251") # ptcp154, also work site:https://lyceum.tom.ru/raspsp/index.php?k=b10&s=1
+    schedule = pd.read_html("https://web.archive.org/web/20221002202403/https://lyceum.tom.ru/raspsp/index.php", encoding="cp1251") # ptcp154, also work site:https://lyceum.tom.ru/raspsp/index.php?k=b10&s=1
 
     datetime_pattern = r'\d{2}\.\d{2}\.\d{4}\. (\d{2}:\d{2}:\d{2})'
-    edit = re.search(datetime_pattern, shedule[4][0][0]).group()
+    edit = re.search(datetime_pattern, schedule[4][0][0]).group()
 
     if last_edit != edit:
         last_edit = edit
         day_of_week = local_date.weekday()
-        print(shedule[5 + day_of_week])
+        print(schedule[5 + day_of_week])
 
     time.sleep(600)
+"""
