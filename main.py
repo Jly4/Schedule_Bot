ad79f0de740d433e1a6b0b09f9cf897d535532f1
Jly4 <iilya.mironov@yandex.ru>
2023-09-05 16:24:44 +0700

    beta?

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import pandas as pd
 import pytz
 from PIL import Image, ImageDraw, ImageFont
 from aiogram import Bot, Dispatcher, types
-from aiogram.types import ContentType, File, Message, MessageEntity
 from aiogram.utils import executor
 
 # import configs
@@ -16,7 +15,7 @@ local_timezone = pytz.timezone('Asia/Tomsk')
 local_date = datetime.now(local_timezone)
 
 # Создаем объекты бота и диспетчера
-bot = Bot(token = configs.token)
+bot = Bot(token=configs.token)
 dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['notify_schedule'])
@@ -25,15 +24,17 @@ async def send_photo(message: types.Message):
     print('schedule printing started')
     # schedule parsing
     while True:
-        schedule = pd.read_html("https://lyceum.tom.ru/raspsp/index.php?k=b10&s=1", keep_default_na=False, encoding="cp1251") # ptcp154, also work
+        schedule = pd.read_html("https://lyceum.tom.ru/raspsp/index.php?k=b11&s=1", keep_default_na=True, encoding="cp1251") # ptcp154, also work
         datetime_pattern = r'\d{2}\.\d{2}\.\d{4}\. (\d{2}:\d{2}:\d{2})'
         edit = re.search(datetime_pattern, schedule[3][0][0]).group()
 
         if last_edit != edit:
             last_edit = edit
             day_of_week = local_date.weekday()
+            if local_date.hour > 15:
+                day_of_week += 1
             text_day_of_week = {0:'Понедельник', 1:'Вторник', 2:'Среда', 3:'Четверг', 4:'Пятница', 5:'Суббота', 6:'Понедельник'}[day_of_week]
-            schedule = schedule[4 + day_of_week % 5 - day_of_week // 5].iloc[:, 1:]
+            schedule = schedule[4 + day_of_week % 5 - day_of_week // 5].fillna('-').iloc[:, 1:]
             column_widths = [max(schedule[col].astype(str).apply(len).max(), len(str(col))) for col in schedule.columns]
 
             # Создаем изображение
@@ -61,13 +62,16 @@ async def send_photo(message: types.Message):
 
             # Сохраняем изображение
             image.save('temp/table_image.png')
+            try:
+                # Загрузите фотографию, которую хотите отправить (замените 'photo.jpg' на путь к вашей фотографии)
+                with open('temp/table_image.png', 'rb') as photo_file:
+                    # Отправьте фотографию пользователю
+                    await message.reply_photo(photo_file, caption=f'{text_day_of_week} - Последние изменение: [{last_edit}]\n\n')
+            except:
+                continue
 
-            # Загрузите фотографию, которую хотите отправить (замените 'photo.jpg' на путь к вашей фотографии)
-            with open('temp/table_image.png', 'rb') as photo_file:
-                # Отправьте фотографию пользователю
-                await message.reply_photo(photo_file, caption=f'{text_day_of_week} - Последние изменение: [{last_edit}]\n\n')
 
-        time.sleep(600)
+        time.sleep(1200)
 
 if __name__ == '__main__':
     print('Bot started')
