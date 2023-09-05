ef82f31c95499b1720257962d04b37978d74db8a
Jly4 <iilya.mironov@yandex.ru>
2023-09-05 22:31:58 +0700

    amvera_config

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ local_timezone = pytz.timezone('Asia/Tomsk')
 local_date = datetime.now(local_timezone)
 
 # Создаем объекты бота и диспетчера
-bot = Bot(token=token)
+bot = Bot(token=config.token)
 dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['notify_schedule'])
