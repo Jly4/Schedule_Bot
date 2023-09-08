8282b8c556e899b5a28df5a50a4c4dbb97cd71c7
Jly4 <iilya.mironov@yandex.ru>
2023-09-08 19:27:20 +0700

    v1.2 Fix

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
+import logging
 import re
 import time
-import pytz
-import logging
-import pandas as pd
 from datetime import datetime
+import pandas as pd
+import pytz
 from PIL import Image, ImageDraw, ImageFont
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
-#from configs import config
+# from configs import config
 from configs import tconfig as config
-
 
 # log config
 basic_log = False
@@ -30,16 +29,16 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands=['notify_schedule'])
 async def send_photo(message: types.Message):
 
-    # Получаем текущую локальную дату и время
-    local_timezone = pytz.timezone('Asia/Tomsk')
-    local_date = datetime.now(local_timezone)
-
     datetime_pattern = r'\d{2}\.\d{2}\.\d{4}\. (\d{2}:\d{2}:\d{2})' # паттерн  для поиска даты последн. изменения в датафрейме
     Last_update = '' # Переменная последнего изменения
     print('schedule printing started')
 
     # получение расписания
     while True:
+        # Получаем текущую локальную дату и время
+        local_timezone = pytz.timezone('Asia/Tomsk')
+        local_date = datetime.now(local_timezone)
+
         schedule = pd.read_html("https://lyceum.tom.ru/raspsp/index.php?k=b11&s=1", keep_default_na=True, encoding="cp1251") # ptcp154, also work
         last_schedule_edit = re.search(datetime_pattern, schedule[3][0][0]).group() # Получаем нужный датафрейм из списка датафреймов сайта
 
@@ -51,17 +50,18 @@ async def send_photo(message: types.Message):
                 day_of_week += 1
 
             text_day_of_week = {0:'Понедельник', 1:'Вторник', 2:'Среда', 3:'Четверг', 4:'Пятница', 5:'Суббота', 6:'Понедельник'}[day_of_week]
-            schedule = schedule[4 + day_of_week % 5 - day_of_week // 5].fillna('-').iloc[:, 1:]
+            schedule = schedule[3 + (day_of_week + 1) % 7].fillna('-').iloc[:, 1:]
             column_widths = [max(schedule[col].astype(str).apply(len).max() + 1, len(str(col))) for col in schedule.columns]
 
             # Создаем изображение
             width = sum(column_widths) * 10  # Множитель 10 для более читаемого изображения
-            height = len(schedule) * 30  # Высота строки
+            height = (9 if len(schedule) != 4 else 8) * 30  # Высота строки
             image = Image.new("RGB", (width, height), (255, 255, 143)) # Создаем картинку с фоном с заданным цветом
             draw = ImageDraw.Draw(image)
 
-            # Задаем моноширинный шрифт
-            font = ImageFont.truetype("arial.ttf", 18)
+            # Создаем шрифт по умолчанию для PIL
+            #font = ImageFont.truetype("DejaVuSans.ttf", 18) # для linux
+            font = ImageFont.truetype("arial.ttf", 18) # для винды
 
             # Устанавливаем начальные координаты для текста
             x, y = 10, 10
